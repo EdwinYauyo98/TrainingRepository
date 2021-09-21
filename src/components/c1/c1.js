d071babb22d0195643764be90ddf78869f300bd0
Sergio Yepez <sergio.2435@hotmail.com>
2021-09-20 20:15:01 -0500

    Services-Presentation

--- src/components/c1/c1.js
+++ src/components/c1/c1.js
@@ -4,6 +4,56 @@ export default function c1() {
     return (
         <div>
             
+        <div className="Contenedor-General">
+                        <h1 className="tittle">Te ayudamos</h1>
+                        <h1 className="tittle1">en estos desafios</h1>
+                    <center>
+                        <div className="Container-line">
+                                
+                        </div>
+                    </center>
+            <div className="cuadro-general">
+                <div className="cuadro">
+                    <div className="cajaimg">
+                        
+                    </div>
+                    <div className="cajatxt">
+                            <h3 className="text">Creacion de marca grafico</h3>
+                            <h3 className="text">e identidad corporativa</h3>
+                            <p className="comentario">Desde el nombre, estructura hasta </p>
+                            <p className="comentario2">las aplicaciones de como se vera</p>
+                            <p className="comentario3">tu marca.</p>
+                    </div>
+                </div>
+                <div className="cuadro">
+                   
+                    <div className="cajatxt">
+                            <h3 className="text">Creacion de marca grafico</h3>
+                            <h3 className="text">e identidad corporativa</h3>
+                            <p className="comentario">Desde el nombre, estructura hasta </p>
+                            <p className="comentario2">las aplicaciones de como se vera</p>
+                            <p className="comentario3">tu marca.</p>
+                    </div>
+                </div>        
+                <div className="cuadro">
+                    
+                        <div className="cajatxt">
+                            <h3 className="text">Creacion de marca grafico</h3>
+                            <h3 className="text">e identidad corporativa</h3>
+                            <p className="comentario">Desde el nombre, estructura hasta </p>
+                            <p className="comentario2">las aplicaciones de como se vera</p>
+                            <p className="comentario3">tu marca.</p>
+                        </div>
+                </div>
+            </div>  
+
+            <center>
+            <a href="#" className="enlace">más proyectos</a>        
+            </center>
+            <div className="blanco"></div>
+        </div>
+    
+    )
         </div>
     )
 }
