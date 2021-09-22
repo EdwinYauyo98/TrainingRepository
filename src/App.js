4fa791449a0cec240da9e43ab9fddbb1b0f6d2f9
Sergio Yepez <sergio.2435@hotmail.com>
2021-09-21 19:23:36 -0500

    FinalSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 // import './App.css';
-
+import { FinalSection } from './components/FinalSection/FinalSection';
 function App() {
   return (
     <div className="App">
+      <FinalSection/>
     </div>
   );
 }
